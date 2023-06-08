179d49040cd41ca5650b12570e8bbe02ad311a5f
Alexis Haldy <alexishaldy@gmail.com>
2023-06-08 11:40:34 +0200

    Add comments to input to be clearest

--- include/Input.h
+++ include/Input.h
@@ -9,11 +9,41 @@ namespace Input
 
     void Update();
 
+    /**
+     * @brief Returns true if the key was pressed this frame.
+     * @param key The key to check. Example: KB_KEY_A
+     * @return
+     */
     bool IsKeyPressed(int key);
+    /**
+     * @brief Returns true if the key was released this frame.
+     * @param key The key to check. Example: KB_KEY_A
+     * @return
+     */
     bool IsKeyReleased(int key);
+    /**
+     * @brief Returns true if the key is being held down.
+     * @param key The key to check. Example: KB_KEY_A
+     * @return
+     */
     bool IsKeyHeld(int key);
 
+    /**
+     * @brief Returns true if the mouse button was pressed this frame.
+     * @param button The mouse button to check. Example: MOUSE_LEFT
+     * @return
+     */
     bool IsMouseButtonPressed(int button);
+    /**
+     * @brief Returns true if the mouse button was released this frame.
+     * @param button The mouse button to check. Example: MOUSE_LEFT
+     * @return
+     */
     bool IsMouseButtonReleased(int button);
+    /**
+     * @brief Returns true if the mouse button is being held down.
+     * @param button The mouse button to check. Example: MOUSE_LEFT
+     * @return
+     */
     bool IsMouseButtonHeld(int button);
 }
